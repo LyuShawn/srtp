74df33f0b7dede2e9b565286e4fec7ae4a1a167a
Savona <905899183@qq.com>
2021-04-05 13:22:33 +0800

    IV曲线表初始化

---
+++ web/src/components/timeIV/index.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="detail">
+            <div class="close" @click="closeDetail"><i class="el-icon-close" style="transform:scale(3);"></i></div>
+            <div id="IVChart" :style="{ width: '100%', height: '80%' }" style="margin-top:50px;" ></div>
+    </div>
+</template>
+<style>
+.detail{
+    background:#fff;
+    width:70%;
+    height:70%;
+    position:fixed;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+    border-radius:10px;
+     box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)
+}
+.close{
+    float:right;
+    
+    margin:20px;
+    transition:transform 0.2s ease-in-out;
+}
+.close:hover{
+     transform:rotate(180deg);
+}
+</style>
+<script>
+export default{
+    name: 'detailIV',
+    methods:{
+        closeDetail(){
+            this.$emit('isClosed', false);
+        },
+        initEChart(){
+            var _this=this;
+                //this.$root => app
+            let myChart = this.$root.echarts.init(
+                document.getElementById("IVChart")
+            );
+            myChart.on('click',function(params){
+                _this.showIV = true;
+            });
+            // 绘制图表
+            myChart.setOption({
+                title: { text: "板1" },
+                tooltip: {},
+                legend: {
+                show:false,
+                },
+                xAxis: {
+                boundaryGap: false,
+                data: ["12-3", "12-4", "12-5", "12-6", "12-7", "12-8"],
+                },
+                yAxis: {
+                boundaryGap: false,
+                
+                data: [" ","正常","开路","短路","阴影",""],
+                },
+                series: [
+                {
+                    name: "板1",
+                    type: "line",
+                    data: [0, 1, 2, 3, "阴影", 0],
+                },
+                ],
+                backgroundColor: 'rgba(255,255,255,0.8)',
+            });
+         },
+    },
+    mounted(){
+        this.initEChart();
+    }
+}
+</script>
--- web/src/views/index/index.vue
+++ web/src/views/index/index.vue
@@ -1,34 +1,70 @@
 <template>
     <div>
-        <div id="myChart" :style="{ width: '300px', height: '300px' }"></div>
+        <div id="myChart" :style="{ width: '800px', height: '300px' }"></div>
+        <transition  name="el-fade-in"><div v-if="showIV"><timeIV @isClosed="closeDeatil" /></div></transition>
     </div>
-</template>
 
+</template>
+<style>
+</style>
 <script>
+import timeIV from '@/components/timeIV'
 export default {
   name: 'index',
-  mounted() {
-    //this.$root => app
-    let myChart = this.$root.echarts.init(
-      document.getElementById("myChart")
-    );
-    // 绘制图表
-    myChart.setOption({
-      title: { text: "总用户量" },
-      tooltip: {},
-      xAxis: {
-        data: ["12-3", "12-4", "12-5", "12-6", "12-7", "12-8"],
-      },
-      yAxis: {},
-      series: [
-        {
-          name: "用户量",
-          type: "line",
-          data: [5, 20, 36, 10, 10, 20],
+  components: {
+    timeIV,
+  },
+  data() {
+    return{
+      showIV: true,
+    }
+  },
+  methods:{
+    closeDeatil(flag){
+      this.showIV = false;
+    },
+
+    initEChart(){
+      var _this=this;
+        //this.$root => app
+      let myChart = this.$root.echarts.init(
+        document.getElementById("myChart")
+      );
+      myChart.on('click',function(params){
+        _this.showIV = true;
+      });
+      // 绘制图表
+      myChart.setOption({
+        title: { text: "板1" },
+        tooltip: {},
+        legend: {
+          show:false,
         },
-      ],
-      backgroundColor: 'rgba(255,255,255,0.8)',
-    });
+        xAxis: {
+          boundaryGap: false,
+          data: ["12-3", "12-4", "12-5", "12-6", "12-7", "12-8"],
+        },
+        yAxis: {
+          boundaryGap: false,
+          
+          data: [" ","正常","开路","短路","阴影",""],
+        },
+        series: [
+          {
+            name: "板1",
+            type: "line",
+            data: [0, 1, 2, 3, "阴影", 0],
+          },
+        ],
+        backgroundColor: 'rgba(255,255,255,0.8)',
+      });
+    },
+
+  },
+  mounted() {
+    this.showID=true;
+    this.initEChart();
+
   },
 }
 </script>
